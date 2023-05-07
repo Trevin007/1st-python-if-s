b1b92a42258ed4200a6cb343990e4f52895fc6c2
Trevin007 <131873760+Trevin007@users.noreply.github.com>
2023-05-07 16:35:11 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+
+beers=int(input("how many girlfriends do trevin ayya have?"))
+vodka=int(input("how many did u see?"))
+drunkness=vodka/beers
+
+print("Drunkness:" + str(drunkness))
+if drunkness >1:
+     print("trevin is a fucking player")
+elif drunkness<1:
+     print("trevin is innocent ")
+else :
+    print("trevin is a fucking player")
+    
